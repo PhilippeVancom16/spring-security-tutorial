55b54606367b2ce9418e848e79d9c7450ef1c46e
Phil <philippe.vancom@gmail.com>
2019-10-30 14:24:42 +0100

    security config (non fini 2)

---
+++ src/main/java/be/afelio/java/software/academy/spring_security_tutorial/security/MyBasicAuthenticationEntryPoint.java
@@ -0,0 +1,6 @@
+package be.afelio.java.software.academy.spring_security_tutorial.security;
+
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+
+public class MyBasicAuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
+}
--- src/main/java/be/afelio/java/software/academy/spring_security_tutorial/security/SecurityConfig.java
+++ src/main/java/be/afelio/java/software/academy/spring_security_tutorial/security/SecurityConfig.java
@@ -1,10 +1,14 @@
 package be.afelio.java.software.academy.spring_security_tutorial.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -13,8 +17,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private MyBasicAuthenticationEntryPoint authenticationEntryPoint;
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user1").password(passwordEncoder().encode("user1Pass"))
+                .authorities("ROLE_USER");
     }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }

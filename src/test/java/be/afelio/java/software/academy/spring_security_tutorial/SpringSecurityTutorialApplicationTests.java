a67eb85d64b366eca1493e30514413d2e8200827
Phil <philippe.vancom@gmail.com>
2019-10-30 13:27:06 +0100

    initial commit

---
+++ src/test/java/be/afelio/java/software/academy/spring_security_tutorial/SpringSecurityTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package be.afelio.java.software.academy.spring_security_tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
